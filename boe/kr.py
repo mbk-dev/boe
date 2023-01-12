f076b3cb7e792a1283fb4c4384be9089bc1e26ab
Sourcery AI <>
2023-01-12 13:56:53 +0000

    'Refactored by Sourcery'

--- boe/kr.py
+++ boe/kr.py
@@ -7,7 +7,7 @@ def get_bank_rate(start_date: pd.Timestamp = pd.Timestamp(1900, 1, 1),
     Get Bank of England main refinancing operations key rate monthly time series.
     """
     start_period = start_date.strftime("%Y-%m-%d")
-    end_period = None if (end_date == None) else end_date.strftime("%Y-%m-%d")
+    end_period = None if end_date is None else end_date.strftime("%Y-%m-%d")
     s = boe.request_data.get_data_frame()
     s.rename("bank_rate", inplace=True)
     return s / 100
